c5744c228ab152040e46b2aacf7f2b5cb6188e80
2019079907 <grace915@hanyang.ac.kr>
2019-09-26 13:46:45 +0900

    first commit

--- hw4-1/string.cpp
+++ hw4-1/string.cpp
@@ -5,17 +5,18 @@ using namespace std;
 
 int main(void) {
 
-	string a,b;
+	string a,b,c;
 	char first;
 	char last;
 
 
 	cin >> a;
 	cin >> b;
+	c = a + b;
 	first = a[0];
 	last = b[b.size()-1];
 
-	cout << a << b << endl;
+	cout << c  << endl;
 	cout << first << endl;
 	cout << last << endl;
 
