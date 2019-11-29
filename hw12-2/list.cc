0d6efc347a5b3b8c089aaa4ac9e61eb3d83cd04a
2019079907 <grace915@hanyang.ac.kr>
2019-11-29 18:34:27 +0900

    hw12-2

---
+++ hw12-2/list.cc
@@ -0,0 +1,153 @@
+#include "list.h"
+
+template<class T>
+List<T>::List() {
+	
+	head = NULL;
+
+}
+template<class T>
+List<T>::List(T* arr, int n_nodes) {
+
+
+head = NULL;
+for(int i = 0; i< n_nodes;i++){
+this->push_back(arr[i]);
+}
+/*
+	if (n_nodes) {
+		head = Node<T>(arr[0]);//arr[0]을 넣고
+		Node<T>* before = head;//그전게 머리가 되고
+		for (int i = 1; i < n_nodes; i++) {
+			before->next = Node<T>(arr[i]);//arr수만큼  넣는데, before의 다음 에 넣고
+			before = before->next;// 그전께 다음꺼가 된다.
+		}
+	}
+	else {
+		this->head = NULL;
+	}
+*/	
+}
+template<class T>
+bool List<T>::isEmpty()const {
+	if (head == NULL)
+		return 1;
+	else
+		return 0;
+}
+
+template<class T>
+void List<T>::insert_at(int idx, T data) {
+	
+	if (idx == 0) {
+		push_front(data);
+		return;
+	}
+	if (head == NULL)
+		return;
+
+	Node<T>* newnode = new Node<T>(data);
+	Node<T>* seek = head;
+	int chk = 0;
+	while (seek != NULL) {
+		chk++;
+		if (chk == idx) {
+			newnode->next = seek->next;
+			seek->next = newnode;
+			break;
+		}
+		seek = seek->next;
+	}
+
+
+}
+template<class T>
+void List<T>::remove_at(int idx) {
+	if (head == NULL) {
+		return;
+	}
+	if (idx == 0) {
+		pop_front();
+		return;
+	}
+	else  /*(idx > 0 && idx <= size() - 1) */{
+		Node <T>* temp = head;
+		Node<T>* target = head;
+		for (int i = 0; i <= idx - 1; i++) {
+			temp = temp->next;
+			target = target->next;
+		}
+		target = target->next;
+		temp->next = target->next;
+		delete target;
+	}
+}
+template<class T>
+void List<T>::pop_back() {
+	if (head == NULL) {
+
+	}
+	else {
+		Node<T>* before = NULL;
+		Node<T>* now = head;
+		for(Node<T>* ptr = head; ptr !=NULL ; ptr = ptr->next){
+			before = now;
+			now = ptr;
+		}
+		before->next = NULL;
+		if (now == head) {
+			head = NULL;
+		}
+		delete now;
+	}
+}
+template<class T>
+void List<T>::push_back(T val) {
+	Node<T>* newNode = new Node<T>(val);
+	if (head == NULL) {
+		head = newNode;
+	}
+	else {
+		Node<T>* now = head;
+		for (Node<T>* ptr = head; ptr != NULL; ptr = ptr->next) {
+			now = ptr;
+		}
+		now->next = newNode;
+	}
+	
+}
+template<class T>
+void List<T>::pop_front() {
+	head = NULL;
+	while (isEmpty()) {
+		head = head->next;
+
+	}
+
+}
+template<class T>
+void List<T>::push_front(T val) {
+	Node<T>* newNode = new Node<T>(val);
+	newNode->next = this->head;
+	this->head = newNode;
+
+}
+template<class T>
+ostream& operator<<(ostream& out, const List<T>& rhs) {
+	Node<T>* seek = rhs.head;
+	while (seek != NULL) {
+		out << seek->data;
+		seek = seek->next;
+
+		if (seek == NULL) {
+
+		}
+		else {
+			out << ", ";
+		}
+	}
+	return out;
+
+}
+
+
---
+++ hw12-2/list.h
@@ -0,0 +1,34 @@
+#pragma once
+#include <iostream>
+using namespace std;
+
+
+template <class T>
+class Node
+{
+public:
+	T data;
+	Node<T>* next;
+	Node(T d) { 
+		data = d; next = NULL;
+	}
+};
+
+template <class T>
+class List {
+private:
+	Node<T>* head;
+public:
+	List(); // create an empty list, head = NULL;
+	List(T* arr, int n_nodes);//create a list with n_nodes and initialize using arr
+	bool isEmpty() const;
+	void insert_at(int idx, T data); // assume 0 <= idx < n_size
+	void remove_at(int idx); // assume 0 <= idx < n_size
+	void pop_back(); // if list is empty, do nothing.
+	void push_back(T val);
+	void pop_front(); // if list is empty, do nothing.
+	void push_front(T val);
+	
+	template<class T>
+	friend ostream& operator<<(ostream& out, const List& rhs);
+};
